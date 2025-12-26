9ee2373518ddc7aba8e05df34043d7f0c686f2c8
Agustin Varela <agustin.varela@mimiquate.com>
2025-12-26 08:50:23 -0300

    add migration model

--- lib/heads_up/incidents/incident.ex
+++ lib/heads_up/incidents/incident.ex
@@ -4,14 +4,28 @@ defmodule HeadsUp.Incidents.Incident do
 
   schema "incidents" do
     field :name, :string
-    field :priority, :integer
-    field :status, Ecto.Enum, values: [:pending, :resolved, :canceled]
+    field :priority, :integer, default: 1
+    field :status, Ecto.Enum, values: [:pending, :resolved, :canceled], default: :pending
     field :description, :string
-    field :image_path, :string
+    field :image_path, :string, default: "/images/placeholder.jpg"
 
     timestamps(type: :utc_datetime)
   end
 
+  @spec changeset(
+          {map(),
+           %{
+             optional(atom()) =>
+               atom()
+               | {:array | :assoc | :embed | :in | :map | :parameterized | :supertype | :try,
+                  any()}
+           }}
+          | %{
+              :__struct__ => atom() | %{:__changeset__ => any(), optional(any()) => any()},
+              optional(atom()) => any()
+            },
+          :invalid | %{optional(:__struct__) => none(), optional(atom() | binary()) => any()}
+        ) :: Ecto.Changeset.t()
   @doc false
   def changeset(incident, attrs) do
     incident
